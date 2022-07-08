915cedf0d5acb6a2efda033387637ee1880feb2c
Calum <calumcmccall@gmail.com>
2022-07-08 15:13:13 +0100

    can play from url input, printing result to console

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from models.game import play_game, result
+from models.game import play_game, play_game_from_url
 from models.player import Player
 
 @app.route('/')
@@ -8,4 +8,9 @@ def index():
     player_1 = Player("Tom", "paper")
     player_2 = Player("Amy", "paper")
     play_game(player_1, player_2)
+    return render_template('index.html')
+
+@app.route('/<player_1_move>/<player_2_move>')
+def game(player_1_move, player_2_move):
+    play_game_from_url(player_1_move, player_2_move)
     return render_template('index.html')
--- models/game.py
+++ models/game.py
@@ -1,5 +1,11 @@
-from unittest import result
 from models.player import Player
+
+winning_player = None
+
+def play_game_from_url(player_1_move, player_2_move):
+    player_1 = Player("player 1", player_1_move)
+    player_2 = Player("player 2", player_2_move)
+    play_game(player_1, player_2)
 
 def play_game(player_1, player_2):
     if player_1.player_move == "rock":
@@ -25,3 +31,5 @@ def play_game(player_1, player_2):
             winning_player = None
 
     return print(f"{winning_player} wins")
+    # return print(player_1.player_name, player_1.player_move)
+
