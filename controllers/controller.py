8c6ba307124cdd160a5cdf09a482e33d047d7b24
Calum <calumcmccall@gmail.com>
2022-07-09 14:43:45 +0100

    tidy up, look sharp

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,14 +1,10 @@
-from flask import redirect, render_template, request, url_for
+from flask import render_template, request
 from app import app
 from models.game import create_computer, play_game, play_game_from_url
 from models.player import Player
 
 @app.route('/')
 def index():
-    # player_1 = Player("Tom", "paper")
-    # player_2 = Player("Amy", "paper")
-    # play_game(player_1, player_2)
-    # return render_template('index.html')
     return render_template('welcome.html')
 
 @app.route('/<player_1_move>/<player_2_move>')
@@ -31,5 +27,4 @@ def play():
     player_submit = Player(player_name, player_move)
     computer = create_computer()
     this_game = play_game(player_submit, computer)
-    # play_game_with_computer(player_submit)
     return render_template('index.html', game_result=this_game)
